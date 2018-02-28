21cf93d02bdf1a135e25662d608b996734fd266a
mattia1989 <mattiadiluca@gmail.com>
2018-02-28 23:16:37 +0100

    comment cleaning

--- DoubleAutoGarage/StatusGarage.h
+++ DoubleAutoGarage/StatusGarage.h
@@ -16,3 +16,4 @@ enum Status_garage
 };
 	
 #endif
+
--- DoubleAutoGarage/TimerObject.h
+++ DoubleAutoGarage/TimerObject.h
@@ -4,13 +4,10 @@
 #ifndef TIMER_OBJECT_H
 #define TIMER_OBJECT_H
 
-<<<<<<< HEAD
 #include "AutomaticGarage.h"
-=======
 #include "StatusGarage.h"
 
 class AutomaticGarage;
->>>>>>> aa648c8ed2f650b40d9111d165c9cd0fd011eda6
 
 class TimerObject {
 private:
